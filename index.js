e00ed08c031bf4dbd79eef63ae534b3f3f02d0d6
Johnathan <johnathan.mnn@gmail.com>
2022-09-01 14:27:25 -0500

    Main selection menu working

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// Import and require mysql2
+// Import and require necessary stuffs
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const fs = require('fs');
 
 // Connect to database
 const db = mysql.createConnection(
@@ -14,9 +16,84 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_tracker_db database.`)
 );
 
-// Query database
-db.query('SELECT * FROM employee', function (err, results) {
-  const transformed = results.reduce((acc, {id, ...x}) => { acc[id] = x; return acc}, {})
-  console.table(transformed)
-});
+const mainMenu = [
+  {
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+      "View All Departments",
+       "View All Roles",
+        "View All Employees",
+         "Add a Department",
+          "Add a Role",
+           "Add an Employee",
+            "Update Employee Role"],
+      name: "mainMenu",
+    }
+  ];
 
+  const addDepartment = [
+    {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+        "View All Departments",
+         "View All Roles",
+          "View All Employees",
+           "Add a Department",
+            "Add a Role",
+             "Add an Employee",
+              "Update Employee Role"],
+        name: "mainMenu",
+      }
+    ];
+
+function askQuestions() {
+return inquirer.prompt(mainMenu)
+.then((response) =>{
+  if (response.mainMenu === "View All Departments" ) {
+    db.query('SELECT * FROM department', function (err, results) {
+      // const transformed = results.reduce((acc, {id, ...x}) => { acc[id] = x; return acc}, {})
+      // console.table(transformed)
+      console.table(results);
+      askQuestions()
+    })
+  } 
+  if (response.mainMenu === "View All Roles" ) {
+    db.query('SELECT * FROM roles', function (err, results) {
+      // const transformed = results.reduce((acc, {id, ...x}) => { acc[id] = x; return acc}, {})
+      // console.table(transformed)
+      console.table(results);
+      askQuestions()
+    })
+  } 
+  if (response.mainMenu === "View All Employees" ) {
+    // Query database to find all employees
+    db.query('SELECT * FROM employee', function (err, results) {
+      // const transformed = results.reduce((acc, {id, ...x}) => { acc[id] = x; return acc}, {})
+      // console.table(transformed)
+      console.table(results);
+      askQuestions()
+    })
+  }
+  if (response.mainMenu === "Add a Department" ) {
+
+    fs.appendFile("./db/seeds/department_seeds_sql", '' , (err) => { 
+      if (err) { 
+        console.log(err); 
+      } 
+    });
+  } 
+  if (response.mainMenu === "Add a Role" ) {
+
+  }
+  if (response.mainMenu === "Add an Employee" ) {
+
+  }  
+  if (response.mainMenu === "Update Employee Role" ) {
+
+  } 
+});
+};
+
+askQuestions();
