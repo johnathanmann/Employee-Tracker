908363c61be2f0d1b7d7702e37c49f38f0eef18d
Johnathan <johnathan.mnn@gmail.com>
2022-09-04 19:39:36 -0500

    Add employee adn update employee working

--- db/schema.sql
+++ db/schema.sql
@@ -12,22 +12,13 @@ CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salery DECIMAL NOT NULL,
-    department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
+    department_id INT
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    roles_id INT ,
-    manager_id INT,
-    FOREIGN KEY (roles_id)
-    REFERENCES roles(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
+    roles VARCHAR(30) ,
+    manager VARCHAR(30)
 );
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import and require necessary stuffs
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const fs = require('fs');
 
 // Connect to database
 const db = mysql.createConnection(
@@ -10,16 +11,11 @@ const db = mysql.createConnection(
     user: 'root',
     // MySQL password
     password: 'Berserk87!',
-    database: 'db/employee_tracker_db'
+    database: 'employee_tracker_db'
   },
   console.log(`Connected to the employee_tracker_db database.`)
 );
 
-// Necessary variables for question asking
-let roles =  db.query("SELECT * FROM roles");
-
-
-// Main set of questions
 const mainMenu = [
   {
       type: "list",
@@ -36,7 +32,22 @@ const mainMenu = [
     }
   ];
 
-// Questions asked when add department is selected
+  const addDepartment = [
+    {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+        "View All Departments",
+         "View All Roles",
+          "View All Employees",
+           "Add a Department",
+            "Add a Role",
+             "Add an Employee",
+              "Update Employee Role"],
+        name: "mainMenu",
+      }
+    ];
+
     const newDepartment = [
       {
         type:"input",
@@ -44,7 +55,7 @@ const mainMenu = [
         name: "department"
       }
     ]
-// Questions for adding a new role
+
   const addRole = [
     {
       type:"input",
@@ -55,39 +66,43 @@ const mainMenu = [
       type:"input",
       message: "What is the roles salary?",
       name: "roleSalary"
-    },
-    {
-      type:"input",
-      message: "What is the roles department?",
-      name: "departmentName"
     }
-    ];
+  ];
 
-    const addEmployee = [
-      {
-        type:"input",
-        message: "What is the employee's first name?",
-        name: "firstName"
-      },
-      {
-        type:"input",
-        message: "What is the employee's last name?",
-        name: "lastName"
-      },
-      {
-        type:"input",
-        message: "What is the employees role?",
-        name: "employeeRole"
-      },
-      {
-        type:"input",
-        message: "Who is the employees manager?",
-        name: "employeeManager"
-      }
-      ];
-
+  const addEmployee = [
+          {
+            type:"input",
+            message: "What is the employee's first name?",
+            name: "firstName"
+          },
+          {
+            type:"input",
+            message: "What is the employee's last name?",
+            name: "lastName"
+          },
+          {
+            type:"input",
+            message: "What is the employees role?",
+            name: "employeeRole"
+          },
+          {
+            type:"input",
+            message: "Who is the employees manager?",
+            name: "employeeManager"
+          }
+          ];
+    
+          const updateEmployee = [
+            {
+              type:"input",
+              message: "What is the id of the employee you want to update?",
+              name: "updateId"
+            }];
+    
 
 function askQuestions() {
+  db.query('SOURCE db/schema.sql', function (err, results) {
+  });
 return inquirer.prompt(mainMenu)
 .then((response) =>{
   if (response.mainMenu === "View All Departments" ) {
@@ -103,8 +118,11 @@ return inquirer.prompt(mainMenu)
       })
   } 
   if (response.mainMenu === "View All Employees" ) {
+    db.query('SOURCE db/employee_seeds.sql', function (err, results) {
+    })
     db.query('SELECT * FROM employee', function (err, results) {
-      console.table(results)
+      console.table(results);
+      askQuestions();
     })
   }
   if (response.mainMenu === "Add a Department" ) {
@@ -117,28 +135,42 @@ return inquirer.prompt(mainMenu)
   }
   if (response.mainMenu === "Add a Role" ) {
     return inquirer.prompt(addRole).then((response) =>{
-      db.query("INSERT INTO roles (title, salary, department) VALUES ('"+response.roleName+"', '"+response.roleSalary+"','"+response.departmentName+"')", function(err, res) {
+      db.query("INSERT INTO roles (title, salary) VALUES ('"+response.roleName+"', '"+response.roleSalary+"')", function(err, res) {
         if (err) throw err;
         askQuestions();
       });
     })
   }
   if (response.mainMenu === "Add an Employee" ) {
-    return inquirer.prompt(addEmployee).then((response) =>{
-      db.query("INSERT INTO employee (first_name, last_name, roles, manager) VALUES ('"+response.firstName+"', '"+response.lastName+"','"+response.employeeRole+"','"+response.employeeManager+"')", function(err, res) {
-        if (err) throw err;
-        askQuestions();
-      });
-    })
-  }  
-  if (response.mainMenu === "Update Employee Role" ) {
+        return inquirer.prompt(addEmployee).then((response) =>{
+          db.query("INSERT INTO employee (first_name, last_name, roles, manager) VALUES ('"+response.firstName+"', '"+response.lastName+"','"+response.employeeRole+"','"+response.employeeManager+"')", function(err, res) {
+            if (err) throw err;
+            askQuestions();
+          });
+        })
+      }  
 
-  } 
+  if (response.mainMenu === "Update Employee Role" ) {
+        // Show all employees so they can find the id of desired employee
+        db.query('SELECT * FROM employee', function (err, results) {
+          console.table(results);
+          return inquirer.prompt(updateEmployee).then((response) =>{
+            db.query("DELETE FROM employee WHERE id= "+response.updateId+" ", function(err, res) {
+              if (err) throw err;
+            });
+            return inquirer.prompt(addEmployee).then((response) =>{
+              db.query("INSERT INTO employee (first_name, last_name, roles, manager) VALUES ('"+response.firstName+"', '"+response.lastName+"','"+response.employeeRole+"','"+response.employeeManager+"')", function(err, res) {
+                if (err) throw err;
+                askQuestions();
+              });
+            })
+          })
+        })
+    
+      } 
 });
 };
 
-// Source schema file
-db.query("SOURCE db/schema.sql", function(err, res) {
-  if (err) throw err;
-});
+
 askQuestions();
+
